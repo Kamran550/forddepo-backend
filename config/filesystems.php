5b4dd3d1abe9f3a947ca7ee7682d48bb85e95934
rzalism-ic <ismayil@intelligentcore.io>
2025-09-12 12:52:43 +0400

    main

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -48,20 +48,20 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-//            'url' => env('AWS_URL'),
-//            'endpoint' => env('AWS_ENDPOINT'),
+            //            'url' => env('AWS_URL'),
+            //            'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
         'images' => [
-           'driver' => 'local',
-           'root' => storage_path('app/images'),
-           'url' => env('APP_URL').'/images',
-           'visibility' => 'public',
+            'driver' => 'local',
+            'root' => storage_path('app/images'),
+            'url' => env('APP_URL') . '/images',
+            'visibility' => 'public',
 
-    ],
+        ],
 
-    /*
+        /*
     |--------------------------------------------------------------------------
     | Symbolic Links
     |--------------------------------------------------------------------------
@@ -72,8 +72,9 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+        'links' => [
+            public_path('storage') => storage_path('app/public'),
+        ],
 
+    ]
 ];
